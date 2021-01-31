4868fe36ab54af7ecc2ba416b1b2c09ebc59c18c
Shahed1998 <shahedchowdhury120@gmail.com>
2021-02-01 01:38:46 +0600

    3rd commit

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const port = 3000;
 const app = express();
-const homeText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Scelerisque eu ultrices vitae auctor eu. Mauris ultrices eros in cursus turpis massa tincidunt dui. 
-Scelerisque purus semper eget duis at tellus at. Faucibus ornare suspendisse sed nisi lacus sed.`;
+const homeText = `This is where I will update my daily routines.`;
 
 const aboutText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -19,13 +16,15 @@ sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Scelerisque eu ultrices vitae auctor eu. Mauris ultrices eros in cursus turpis massa tincidunt dui. 
 Scelerisque purus semper eget duis at tellus at. Faucibus ornare suspendisse sed nisi lacus sed.`;
 
+const globalPost = [];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('Public'));
 app.set('view engine', 'ejs');
 
 //_________________________________________Get requests______________________________________
 app.get('/', (req, res) => {
-  res.render('home', { val: homeText });
+  res.render('home', { val: homeText, globalPost: globalPost });
 });
 
 app.get('/about', (req, res) => {
@@ -40,11 +39,22 @@ app.get('/contact', (req, res) => {
   res.render('contact', { val: contactText });
 });
 
-app.get('/post', (req, res) => {
-  res.render('post');
-});
+// app.get('/post', (req, res) => {
+//   res.render('post', postObj);
+// });
 
 //_________________________________________Post requests______________________________________
+
+app.post('/compose', (req, res) => {
+  const postObj = {
+    title: req.body.title,
+    post: req.body.post,
+  };
+
+  globalPost.push(postObj);
+
+  res.redirect('/');
+});
 
 //_________________________________________Port listening______________________________________
 app.listen(process.env.PORT || port, () => {
